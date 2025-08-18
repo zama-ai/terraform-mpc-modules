2684ac1717d9b006800c2a03db2ab11fd6c21913
Ghislain Cheng <ghislain.cheng@zama.ai>
2025-08-18 18:47:15 +0200

    chore: simplify mpcparty tf module
    Signed-off-by: Ghislain Cheng <ghislain.cheng@zama.ai>

--- modules/mpcparty/main.tf
+++ modules/mpcparty/main.tf
@@ -1,3 +1,20 @@
+# Data source to get eks cluster
+data "aws_eks_cluster" "cluster" {
+  name = var.cluster_name
+}
+
+data "aws_subnet" "cluster_subnets" {
+  for_each = toset(data.aws_eks_cluster.cluster.vpc_config[0].subnet_ids)
+  id       = each.value
+}
+
+locals {
+  private_subnet_ids = [
+    for subnet_id, subnet in data.aws_subnet.cluster_subnets : subnet_id
+    if subnet.map_public_ip_on_launch == false
+  ]
+}
+
 # Create Kubernetes namespace (optional)
 resource "kubernetes_namespace" "mpc_party_namespace" {
   count = var.create_namespace ? 1 : 0
@@ -21,9 +38,12 @@ resource "kubernetes_namespace" "mpc_party_namespace" {
   }
 }
 
-# S3 Bucket for Public Vault Storage
+# ***************************************
+#  S3 Buckets for Vault Public Storage
+# ***************************************
 resource "aws_s3_bucket" "vault_public_bucket" {
   bucket = var.vault_public_bucket_name
+  force_destroy = true
   tags = merge(var.common_tags, {
     "Name"        = var.vault_public_bucket_name
     "Type"        = "public-vault"
@@ -87,10 +107,14 @@ resource "aws_s3_bucket_policy" "vault_public_bucket_policy" {
       }
     ]
   })
+  depends_on = [aws_s3_bucket_public_access_block.vault_public_bucket]
 }
 
-# S3 Bucket for Private Vault Storage
+# ***************************************
+#  S3 Buckets for Vault Private Storage
+# ***************************************
 resource "aws_s3_bucket" "vault_private_bucket" {
+  force_destroy = true
   bucket = var.vault_private_bucket_name
   tags = merge(var.common_tags, {
     "Name"        = var.vault_private_bucket_name
@@ -119,50 +143,47 @@ resource "aws_s3_bucket_public_access_block" "vault_private_bucket" {
   block_public_policy = true
 }
 
-# IRSA Module for secure AWS access from Kubernetes
-module "irsa" {
-  count  = var.create_irsa ? 1 : 0
-  source = "../irsa"
-  
-  cluster_name             = var.cluster_name
-  party_name               = var.party_name
-  k8s_namespace            = var.k8s_namespace
-  k8s_service_account_name = var.k8s_service_account_name
-  name                     = "mpc-party-${var.party_name}"
-  
-  # S3 access policy for the IRSA role
+# ***************************************
+#  IAM Policy for MPC Party
+# ***************************************
+resource "aws_iam_policy" "mpc_aws" {
+  name   = "mpc-${var.cluster_name}-${var.party_name}"
   policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid    = "AllowObjectActions"
-        Effect = "Allow"
-        Action = "s3:*Object"
-        Resource = [
-          "arn:aws:s3:::${aws_s3_bucket.vault_private_bucket.id}/*",
-          "arn:aws:s3:::${aws_s3_bucket.vault_public_bucket.id}/*"
-        ]
-      },
-      {
-        Sid    = "AllowListBucket"
-        Effect = "Allow"
-        Action = "s3:ListBucket"
-        Resource = [
-          "arn:aws:s3:::${aws_s3_bucket.vault_private_bucket.id}",
-          "arn:aws:s3:::${aws_s3_bucket.vault_public_bucket.id}"
-        ]
-      }
-    ]
-  })
-  
-  depends_on = [
-    aws_s3_bucket.vault_private_bucket,
-    aws_s3_bucket.vault_public_bucket,
-    kubernetes_namespace.mpc_party_namespace
+  Version = "2012-10-17"
+  Statement = [
+    {
+      Sid    = "AllowObjectActions"
+      Effect = "Allow"
+      Action = "s3:*Object"
+      Resource = [
+        "arn:aws:s3:::${aws_s3_bucket.vault_private_bucket.id}/*",
+        "arn:aws:s3:::${aws_s3_bucket.vault_public_bucket.id}/*"
+      ]
+    },
+    {
+      Sid    = "AllowListBucket"
+      Effect = "Allow"
+      Action = "s3:ListBucket"
+      Resource = [
+        "arn:aws:s3:::${aws_s3_bucket.vault_private_bucket.id}",
+        "arn:aws:s3:::${aws_s3_bucket.vault_public_bucket.id}"
+      ]
+    }
   ]
+  })
 }
 
-# Create Kubernetes Service Account (only if not using IRSA)
+module "iam_assumable_role_mpc_party" {
+  source                        = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version                       = "5.48.0"
+  provider_url                  = data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer
+  create_role                   = true
+  role_name                     = "mpc-${var.cluster_name}-${var.party_name}-nodegroup"
+  oidc_fully_qualified_subjects = ["system:serviceaccount:${var.k8s_namespace}:${var.k8s_service_account_name}"]
+  role_policy_arns              = [aws_iam_policy.mpc_aws.arn]
+  depends_on = [aws_s3_bucket.vault_private_bucket, aws_s3_bucket.vault_public_bucket, kubernetes_namespace.mpc_party_namespace]
+}
+
 resource "kubernetes_service_account" "mpc_party_service_account" {
   count = var.create_service_account ? 1 : 0
   metadata {
@@ -180,14 +201,15 @@ resource "kubernetes_service_account" "mpc_party_service_account" {
     annotations = merge({
       "terraform.io/module"   = "mpcparty"
       "mpc.io/party-name"     = var.party_name
-      "eks.amazonaws.com/role-arn" = module.irsa[0].iam_role_arn
+      "eks.amazonaws.com/role-arn" = module.iam_assumable_role_mpc_party.iam_role_arn
     }, var.service_account_annotations)
   }
-  
-  depends_on = [kubernetes_namespace.mpc_party_namespace, module.irsa]
+  depends_on = [kubernetes_namespace.mpc_party_namespace, module.iam_assumable_role_mpc_party]
 }
 
-# Create ConfigMap with S3 bucket configuration
+# ***************************************
+#  ConfigMap with S3 bucket configuration
+# ***************************************
 resource "kubernetes_config_map" "mpc_party_config" {
   count = var.create_config_map ? 1 : 0
   
@@ -220,4 +242,54 @@ resource "kubernetes_config_map" "mpc_party_config" {
   }
   
   depends_on = [kubernetes_namespace.mpc_party_namespace, aws_s3_bucket.vault_private_bucket, aws_s3_bucket.vault_public_bucket]
+}
+
+# ***************************************
+#  EKS Managed Node Group
+# ***************************************
+module "eks_managed_node_group" {
+  count = var.create_nodegroup ? 1 : 0
+  source = "terraform-aws-modules/eks/aws//modules/eks-managed-node-group"
+  version = "21.0.6"
+
+  name         = var.nodegroup_name
+  cluster_name = var.cluster_name
+  kubernetes_version = data.aws_eks_cluster.cluster.version
+
+  subnet_ids = local.private_subnet_ids
+
+  cluster_primary_security_group_id = data.aws_eks_cluster.cluster.vpc_config[0].cluster_security_group_id
+  vpc_security_group_ids            = concat(tolist(data.aws_eks_cluster.cluster.vpc_config[0].security_group_ids), var.nodegroup_additional_security_group_ids)
+
+  # Scaling Configuration
+  min_size     = var.nodegroup_min_size
+  max_size     = var.nodegroup_max_size
+  desired_size = var.nodegroup_desired_size
+
+  # Instance Configuration (only when not using launch template)
+  instance_types = var.nodegroup_instance_types
+  capacity_type  = var.nodegroup_capacity_type
+  ami_type       = var.nodegroup_ami_type
+  
+  # Disable settings that conflict with launch template
+  enable_bootstrap_user_data = false
+  
+  # Cluster service CIDR for user data
+  cluster_service_cidr = data.aws_eks_cluster.cluster.kubernetes_network_config[0].service_ipv4_cidr
+  
+  # Disk configuration (only when not using launch template)
+  disk_size = var.nodegroup_disk_size
+
+  # Remote Access Configuration (only when not using launch template)
+  remote_access = var.nodegroup_enable_remote_access ? {
+    ec2_ssh_key               = var.nodegroup_ec2_ssh_key
+    source_security_group_ids = var.nodegroup_source_security_group_ids
+  } : null
+
+  # Labels and Taints
+  labels = var.nodegroup_labels
+  taints = var.nodegroup_taints
+
+  # Tags
+  tags = var.tags
 }
--- modules/mpcparty/variables.tf
+++ modules/mpcparty/variables.tf
@@ -111,4 +111,331 @@ variable "common_tags" {
 }
 
 
+# EKS Node Group Core Configuration
+variable "create_nodegroup" {
+  type        = bool
+  description = "Whether to create an EKS managed node group"
+  default     = false
+}
 
+variable "nodegroup_name" {
+  type        = string
+  description = "Name of the EKS managed node group"
+}
+
+variable "kubernetes_version" {
+  type        = string
+  description = "Kubernetes version for the node group. If not specified, the EKS cluster's Kubernetes version will be used"
+  default     = null
+}
+
+# Scaling Configuration
+variable "nodegroup_min_size" {
+  type        = number
+  description = "Minimum number of instances in the node group"
+  default     = 1
+}
+
+variable "nodegroup_max_size" {
+  type        = number
+  description = "Maximum number of instances in the node group"
+  default     = 10
+}
+
+variable "nodegroup_desired_size" {
+  type        = number
+  description = "Desired number of instances in the node group"
+  default     = 1
+}
+
+# Instance Configuration
+variable "nodegroup_instance_types" {
+  type        = list(string)
+  description = "List of instance types for the node group"
+  default     = ["t3.large"]
+}
+
+variable "nodegroup_capacity_type" {
+  type        = string
+  description = "Type of capacity associated with the EKS Node Group. Valid values: ON_DEMAND, SPOT"
+  default     = "ON_DEMAND"
+}
+
+variable "nodegroup_ami_type" {
+  type        = string
+  description = "Type of Amazon Machine Image (AMI) associated with the EKS Node Group"
+  default     = "AL2_x86_64"
+}
+
+variable "nodegroup_disk_size" {
+  type        = number
+  description = "Disk size in GiB for worker nodes"
+  default     = 20
+}
+
+# Launch Template Configuration
+variable "nodegroup_use_custom_launch_template" {
+  type        = bool
+  description = "Whether to use a custom launch template"
+  default     = true
+}
+
+
+
+# Remote Access Configuration
+variable "nodegroup_enable_remote_access" {
+  type        = bool
+  description = "Whether to enable remote access to the worker nodes"
+  default     = false
+}
+
+variable "nodegroup_ec2_ssh_key" {
+  type        = string
+  description = "EC2 Key Pair name that provides access for SSH communication with the worker nodes"
+  default     = null
+}
+
+variable "nodegroup_source_security_group_ids" {
+  type        = list(string)
+  description = "List of security group IDs allowed for remote access"
+  default     = []
+}
+
+
+variable "nodegroup_additional_security_group_ids" {
+  type        = list(string)
+  description = "List of additional security group IDs to associate with the node group"
+  default     = []
+}
+
+# Labels and Taints
+variable "nodegroup_labels" {
+  type        = map(string)
+  description = "Key-value map of Kubernetes labels applied to the node group"
+  default     = {}
+}
+
+variable "nodegroup_taints" {
+  type        = map(object({
+    key    = string
+    value  = string
+    effect = string
+  }))
+  description = "Map of Kubernetes taints to apply to the node group"
+  default     = {}
+}
+
+# Tags
+variable "tags" {
+  type        = map(string)
+  description = "A map of tags to assign to the resource"
+  default     = {}
+}
+
+# Launch Template Configuration
+variable "nodegroup_create_launch_template" {
+  type        = bool
+  description = "Whether to create a launch template for the node group"
+  default     = false
+}
+
+variable "nodegroup_launch_template_id" {
+  type        = string
+  description = "ID of an existing launch template to use. If provided, create_launch_template will be ignored"
+  default     = null
+}
+
+variable "nodegroup_launch_template_version" {
+  type        = string
+  description = "Launch template version to use. Can be version number, '$Latest', or '$Default'"
+  default     = null
+}
+
+variable "nodegroup_launch_template_name" {
+  type        = string
+  description = "Name of the launch template. If not provided, a name will be generated"
+  default     = null
+}
+
+variable "nodegroup_launch_template_description" {
+  type        = string
+  description = "Description of the launch template"
+  default     = "Launch template for EKS managed node group"
+}
+
+# Instance Configuration for Launch Template
+variable "image_id" {
+  type        = string
+  description = "AMI ID for the launch template. If not provided, latest EKS optimized AMI will be used"
+  default     = null
+}
+
+variable "nodegroup_launch_template_instance_type" {
+  type        = string
+  description = "Instance type for the launch template. Cannot be used with instance_types in node group"
+  default     = null
+}
+
+variable "key_name" {
+  type        = string
+  description = "Key pair name for SSH access to instances"
+  default     = null
+}
+
+variable "nodegroup_security_group_ids" {
+  type        = list(string)
+  description = "List of security group IDs to associate with instances. Will be combined with cluster security group"
+  default     = []
+}
+
+# Block Device Mappings
+variable "block_device_mappings" {
+  type = list(object({
+    device_name = string
+    ebs = optional(object({
+      volume_size           = optional(number, 20)
+      volume_type           = optional(string, "gp3")
+      iops                  = optional(number)
+      throughput            = optional(number)
+      encrypted             = optional(bool, true)
+      kms_key_id            = optional(string)
+      delete_on_termination = optional(bool, true)
+      snapshot_id           = optional(string)
+    }))
+    no_device    = optional(string)
+    virtual_name = optional(string)
+  }))
+  description = "Block device mappings for the launch template"
+  default = [
+    {
+      device_name = "/dev/xvda"
+      ebs = {
+        volume_size           = 20
+        volume_type           = "gp3"
+        encrypted             = true
+        delete_on_termination = true
+      }
+    }
+  ]
+}
+
+# Instance Metadata Options
+variable "nodegroup_launch_template_metadata_options" {
+  type = object({
+    http_endpoint               = optional(string, "enabled")
+    http_tokens                 = optional(string, "required")
+    http_put_response_hop_limit = optional(number, 2)
+    instance_metadata_tags      = optional(string, "disabled")
+  })
+  description = "Instance metadata options for the launch template"
+  default = {
+    http_endpoint               = "enabled"
+    http_tokens                 = "required"
+    http_put_response_hop_limit = 2
+    instance_metadata_tags      = "disabled"
+  }
+}
+
+# User Data Configuration
+variable "nodegroup_launch_template_user_data_base64" {
+  type        = string
+  description = "Base64-encoded user data for the launch template. If not provided, default EKS bootstrap user data will be used"
+  default     = null
+}
+
+variable "nodegroup_enable_bootstrap_user_data" {
+  type        = bool
+  description = "Whether to enable default EKS bootstrap user data when using custom launch template"
+  default     = true
+}
+
+variable "nodegroup_pre_bootstrap_user_data" {
+  type        = string
+  description = "User data commands to run before EKS bootstrap script"
+  default     = ""
+}
+
+variable "nodegroup_post_bootstrap_user_data" {
+  type        = string
+  description = "User data commands to run after EKS bootstrap script"
+  default     = ""
+}
+
+variable "nodegroup_bootstrap_extra_args" {
+  type        = string
+  description = "Additional arguments to pass to the EKS bootstrap script"
+  default     = ""
+}
+
+# CPU and Credit Specification
+variable "nodegroup_launch_template_cpu_options" {
+  type = object({
+    core_count       = optional(number)
+    threads_per_core = optional(number)
+    amd_sev_snp      = optional(string)
+  })
+  description = "CPU options for the launch template"
+  default     = null
+}
+
+variable "nodegroup_launch_template_credit_specification" {
+  type = object({
+    cpu_credits = string
+  })
+  description = "Credit specification for burstable performance instances"
+  default     = null
+}
+
+# Network Interfaces (alternative to security_group_ids)
+variable "nodegroup_launch_template_network_interfaces" {
+  type = list(object({
+    associate_public_ip_address = optional(bool, false)
+    delete_on_termination       = optional(bool, true)
+    device_index               = optional(number, 0)
+    security_groups            = optional(list(string))
+    subnet_id                  = optional(string)
+  }))
+  description = "Network interfaces for the launch template. Cannot be used with security_group_ids"
+  default     = []
+}
+
+# Additional Launch Template Settings
+variable "node_group_launch_template_ebs_optimized" {
+  type        = bool
+  description = "Whether to enable EBS optimization"
+  default     = true
+}
+
+variable "nodegroup_launch_template_monitoring" {
+  type = object({
+    enabled = bool
+  })
+  description = "Monitoring configuration for instances"
+  default = {
+    enabled = true
+  }
+}
+
+# Removed placement variable as it is not supported by the EKS Node Group module
+#variable "placement" {
+#  type = object({
+#    affinity          = optional(string)
+#    availability_zone = optional(string)
+#    group_name        = optional(string)
+#    host_id           = optional(string)
+#    tenancy           = optional(string)
+#  })
+#  description = "Placement configuration for instances"
+#  default     = null
+#}
+#
+#variable "tag_specifications" {
+#  type = list(object({
+#    resource_type = string
+#    tags          = map(string)
+#  }))
+#  description = "Tag specifications for resources created by the launch template"
+#  default     = []
+#}
+#
+#
