deee5e39e72e7deb1b81304bb7f3f9804122c01d
Ghislain Cheng <ghislain.cheng@zama.ai>
2025-08-27 05:14:35 +0200

    chore: Prepare release v0.1.2
    Signed-off-by: Ghislain Cheng <ghislain.cheng@zama.ai>

--- examples/mpc-network-provider/main.tf
+++ examples/mpc-network-provider/main.tf
@@ -4,7 +4,7 @@ data "aws_region" "current" {}
 
 # Deploy VPC endpoints for the created NLBs (optional, only in provider mode)
 module "vpc_endpoint_provider" {
-  source = "git::git@github.com:zama-ai/terraform-mpc-modules.git//modules/vpc-endpoint-provider?ref=v0.1.1"
+  source = "git::git@github.com:zama-ai/terraform-mpc-modules.git//modules/vpc-endpoint-provider?ref=v0.1.2"
 
   # Network environment configuration
   network_environment      = var.network_environment
