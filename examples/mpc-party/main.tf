7c6bd61f56a7d1bd48aa2d45cab77b042e865e8c
Ghislain Cheng <ghislain.cheng@zama.ai>
2025-10-09 12:48:28 +0200

    refactor(mpc-party): update nodegroup AMI configuration and add variable for AMI ID
    Signed-off-by: Ghislain Cheng <ghislain.cheng@zama.ai>

--- examples/mpc-party/main.tf
+++ examples/mpc-party/main.tf
@@ -66,10 +66,11 @@ module "mpc_party" {
   kms_deletion_window_in_days             = var.kms_deletion_window_in_days
   nodegroup_additional_security_group_ids = var.nodegroup_additional_security_group_ids
   nodegroup_enable_ssm_managed_instance   = var.nodegroup_enable_ssm_managed_instance
+
   # Custom AMI ID for the node group
-  nodegroup_ami_id = "ami-085d6a1add83c50f3"
-  # nodegroup_use_latest_ami_release_version = var.nodegroup_use_latest_ami_release_version
-  # nodegroup_ami_release_version            = var.nodegroup_ami_release_version
+  nodegroup_ami_id                         = var.nodegroup_ami_id
+  nodegroup_use_latest_ami_release_version = var.nodegroup_ami_id != null ? null : var.nodegroup_use_latest_ami_release_version
+  nodegroup_ami_release_version            = var.nodegroup_ami_id != null ? null : var.nodegroup_ami_release_version
 
   # Nitro Enclaves Configuration
   kms_enabled_nitro_enclaves         = var.kms_enabled_nitro_enclaves
