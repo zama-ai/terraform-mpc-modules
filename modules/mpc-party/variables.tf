51822a4a37e9be96997048f70a69f829554f37ba
Frederic Egmorte <frederic.egmorte@zama.ai>
2025-11-10 14:54:44 +0100

    fix: add default null value

--- modules/mpc-party/variables.tf
+++ modules/mpc-party/variables.tf
@@ -33,6 +33,7 @@ variable "party_name" {
 variable "mpc_party_role_name" {
   type        = string
   description = "The name of the MPC party role"
+  default     = null
   validation {
     condition     = length(var.mpc_party_role_name) <= 64
     error_message = "MPC party role name must be 64 characters or less."
@@ -42,6 +43,7 @@ variable "mpc_party_role_name" {
 variable "connector_role_name" {
   type        = string
   description = "The name of the connector role"
+  default     = null
   validation {
     condition     = length(var.connector_role_name) <= 64
     error_message = "Connector role name must be 64 characters or less."
